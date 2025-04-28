a21a899101aedb85f3af824579dd2a4881db1f3b
Eren Karslı <karslieren61@gmail.com>
2025-04-29 00:51:39 +0300

    npm

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Greeting from './components/Greeting';
+
+ function App() {
+   return (
+     <div className="App" style={{ textAlign: 'center', marginTop: '2rem' }}>
+       <h1>Merhaba React!</h1>
+       <Greeting />
+     </div>
+   );
+ }
---
+++ src/components/Greeting.jsx
@@ -0,0 +1,8 @@
+// src/components/Greeting.jsx
+import React from 'react';
+
+const Greeting = () => {
+  return <h2>Ben ayrı bir bileşenim!</h2>;
+};
+
+export default Greeting;
